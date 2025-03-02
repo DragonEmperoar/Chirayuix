21e08c3d6794586c649d5dc0bf74f8c50a0490bb
DragonEmperoar <chirayu1264@gmail.com>
2025-03-02 15:10:38 +0530

    Update Menu.jsx

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -25,58 +25,59 @@ import Image24 from '../../assets/3D Buiseness Card.webp';
 
 
 
+
 const Menu = [
     { id: 1,
          title: "Noise Redesign", 
          category: ["UI/UX"], 
-         pdf: "/assets/Noise Final render.pdf", 
+         pdf: "https://dl.dropboxusercontent/scl/fi/2fjk493a2q9f7sbr0mhyu/Noise-Final-render.pdf?rlkey=hj252hnhsk39s8m3zslgdri7z&st=aj9ob9xv&dl=0", 
          thumbnail: Image11 },
 
     { id: 2,
          title: "Cantina",
           category: ["Brand Identity & Marketing"],
-           pdf: "/assets/cantina final.pdf",
+           pdf: "https://dl.dropboxusercontent.com/scl/fi/ogd7se3281x0xf3sr634p/Business.pdf?rlkey=okjfcn77g4ag675rjdgqksz35&st=g1agntez&dl=0",
             thumbnail: Image12 },
     { id: 3,
          title: "Holy Mole",
           category: ["Brand Identity & Marketing"],
-           pdf: "/assets/Holy Mole final.pdf",
+           pdf: "https://dl.dropboxusercontent/scl/fi/rbqizq14jwf0kyxdj8is3/Holy-Mole-Final.pdf?rlkey=y1axc61136bdidl8of8tzg97u&st=xfu4r59e&dl=0",
             thumbnail: Image13 },
             
     { id: 4,
          title: "Business Card Mockup",
           category: ["Graphic"],
-           pdf: "/assets/Buiseness Card.pdf",
+           pdf: "https://dl.dropboxusercontent.com/scl/fi/ogd7se3281x0xf3sr634p/Business.pdf?rlkey=okjfcn77g4ag675rjdgqksz35&st=zbxm8xuf&dl=0",
             thumbnail: Image14 },
 
     { id: 5,
          title: "Snapdeal",
           category: ["UI/UX"],
-           pdf: "/assets/snapdeal final.pdf",
+           pdf: "https://dl.dropboxusercontent/scl/fi/ooitat746lsjfqfro9qg6/snapdeal-final.pdf?rlkey=opqdwpjr25k20pezxy1acezbl&st=j94phnrf&dl=0",
             thumbnail: Image15 },
 
     { id: 6,
          title: "Illustration",
           category: ["Graphic"],
-           pdf: "/assets/g6643.pdf",
+           pdf: "https://dl.dropboxusercontent/scl/fi/fhwj4czpefvqbgs72klec/g6643.pdf?rlkey=6zepoq7fepd6q08t0pvcyt40t&st=zwtf1pns&dl=0",
             thumbnail: Image16 },
 
     { id: 7,
          title: "HDFC Bank",
           category: ["UI/UX"],
-           pdf: "/assets/hdfc.pdf",
+           pdf: "https://dl.dropboxusercontent/scl/fi/xx29n0ph4h0cbx0075f2i/hdfc.pdf?rlkey=wotslbtsx6whr632tplcsuj60&st=fsw4tk8k&dl=0",
             thumbnail: Image17 },
 
     { id: 8,
          title: "SoonLust",
           category: ["UI/UX"],
-           pdf: "/assets/soonlust.pdf",
+           pdf: "https://dl.dropboxusercontent/scl/fi/ez4x7z072ku3h99l8ja3q/soonlust.pdf?rlkey=q7onyq90ts4kb6ewz6ukwwm7b&st=hs47xvcn&dl=0",
             thumbnail: Image18 },
 
     { id: 9,
          title: "Paired Pixel",
           category: ["UI/UX"],
-           pdf: "/assets/paired pixel.pdf",
+           pdf: "https://dl.dropboxusercontent/scl/fi/k1wizx8t63vs8gnbw7ima/paired-pixel.pdf?rlkey=ejzmnp17eoz2w07y8v5rzs7cy&st=96o05zp3&dl=0",
             thumbnail: Image19 },
 
     { id: 10,
@@ -88,26 +89,26 @@ const Menu = [
     { id: 11,
          title: "Dron-Acharya",
           category: ["UI/UX"],
-           pdf: "/assets/Dron-Acharya.pdf",
+           pdf: "https://dl.dropboxusercontent/scl/fi/5bl11z225wgw5e2gvc27r/Dron-Acharya.pdf?rlkey=waay1iego5hjwbm3jaif4nkqp&st=3mlnbfxf&dl=0",
             thumbnail: Image21
     },
 
     { id: 12,
          title: "1st Try Reel",
           category: ["Video Editing"],
-           mp4: "/assets/1st-try-reel.mp4",
+           mp4: "https://dl.dropboxusercontent/scl/fi/satwu43h44tnwjcnl2i6x/1-reel.mp4?rlkey=hqmp8rpzyqnn68lfi6aem3a5o&st=rsjp3ryw&dl=0",
             thumbnail: Image23 },
 
     { id: 13,
          title: "Chill Out Architecture",
           category: ["3D Modelling"],
-           pdf: "/assets/Chill Out Architecturee.pdf",
+           pdf: "https://dl.dropboxusercontent/scl/fi/fpvforza3gv64hbbx2bij/Chill-Out-Architecturee.pdf?rlkey=svlu1o520hlq7grvhdfn4cgge&st=7739gp63&dl=0",
             thumbnail: Image22 },
 
     { id: 14,
          title: "3D Business Card",
           category: ["3D Modelling"],
-           mp4: "/assets/last 11.mp4",
+           mp4: "https://dl.dropboxusercontent/scl/fi/bk2ko8k7uhx9h0oyfdefa/last-11.mp4?rlkey=zzhqxeubyogwyvjjwf0nce37q&st=m1e1clco&dl=0",
             thumbnail: Image24 },
 ];
 
